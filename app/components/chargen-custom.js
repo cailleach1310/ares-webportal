6ff9d6e6e807f37254805d399f9d97ab2e3fe9eb
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-15 14:11:59 +0200

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -19,7 +19,6 @@ export default Component.extend({
   onUpdate: function() {
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
     // 
-    return { marque: this.get('char.marque') };
-    return { scion: this.get('char.scion') };
+    return { marque: this.get('char.marque'), scion: this.get('char.scion') };
   }
 });
