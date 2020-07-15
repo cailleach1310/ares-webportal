73a34070f77a3a4c1721f8fd762cebb6fcc72cdf
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-15 13:53:37 +0200

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -8,18 +8,13 @@ export default Component.extend({
     this.set('updateCallback', function() { return self.onUpdate(); } );
   },
  
-// scions = ("Naamah","Eisheth","Camael"),
-// scions: function() {
-//   let list = [];
-//   this.get(custom.scions).forEach(function(g) {
-//     list.push({ value: g });
-//    });
-//  return list;
-//},
-  
-scionChanged(val) {
-   this.set('model.char.custom.scion.value', val.value);
-},
+  scions: computed(function() {
+     let list = [];
+     this.get('model.custom.scions').forEach(function(g) {
+       list.push({ value: g });
+     });
+     return list;
+  }),
 
   onUpdate: function() {
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
