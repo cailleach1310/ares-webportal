6d05d383fc24a03adde2cab2c2ce7ad0ce7ab5de
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-20 22:43:45 +0200

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -28,7 +28,7 @@ export default Component.extend({
 
   actions: {      
            scionChanged(val) {
-           this.set('model.char.custom.scion', val);
+           this.set('model.char.custom.scion.value', val.value);
         },
    },
 
