723c1d43bb75d54a3ecdf31d85ce30c64efbac66
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-22 17:11:31 +0200

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { computed, action } from '@ember/object';
+//import { computed, action } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
@@ -9,27 +9,28 @@ export default Component.extend({
     this.set('updateCallback', function() { return self.onUpdate(); } );
   },
 
-  scions: computed(function() {
-    let list = [];
-    let scionlist = ["Anael", "Azza", "Camael", "Eisheth", "Elua", "Kushiel", "Naamah", "Shemhazai"];
-     scionlist.forEach(function(s) {
-       list.push({ value: s });
-     });
-     return list;
- }),
+//  scions: computed(function() {
+//    let list = [];
+//    let scionlist = ["Anael", "Azza", "Camael", "Eisheth", "Elua", "Kushiel", "Naamah", "Shemhazai"];
+//     scionlist.forEach(function(s) {
+//       list.push({ value: s });
+//     });
+//     return list;
+// }),
 
   onUpdate: function() {
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
     // 
-    return { marque: this.get('char.custom.marque'), 
-            scion: this.get('char.custom.scion') };
+//    return { marque: this.get('char.custom.marque'), 
+//            scion: this.get('char.custom.scion') };
+    return { marque: this.get('char.custom.marque') };
   },
 
 
-  actions: {      
-           scionChanged(val) {
-           this.set('char.custom.scion', val.value);
-        },
-   },
+//  actions: {      
+//           scionChanged(val) {
+//           this.set('char.custom.scion', val.value);
+ //       },
+ //  },
 
 });
