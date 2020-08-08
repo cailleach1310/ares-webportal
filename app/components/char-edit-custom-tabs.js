4f9b8ea56df94b05a1e0c9cb88c87caa0748cb71
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-08-08 08:59:58 +0200

    Update char-edit-custom-tabs.js

--- app/components/char-edit-custom-tabs.js
+++ app/components/char-edit-custom-tabs.js
@@ -3,3 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: ''
 });
+
+is_adept: computed(function() {
+      return this.get('model.char.fs3.fs3_advantages').any(e => e == 'connections');
+    }),
