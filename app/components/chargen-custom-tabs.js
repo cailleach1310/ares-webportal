bb01c51ada9687ac7a5203dd7dc3a6756465b4a8
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-08-08 13:33:22 +0200

    Update chargen-custom-tabs.js

--- app/components/chargen-custom-tabs.js
+++ app/components/chargen-custom-tabs.js
@@ -2,7 +2,7 @@ import { computed } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
-  tagName: ''
+  tagName: '',
   
   has_connections: computed(function() {
       return (this.get('model.char.fs3.fs3_advantages').any(e => e == 'connections'));
