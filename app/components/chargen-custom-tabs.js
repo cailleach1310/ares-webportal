7ad5f5ab171a317c7f44204681799732b8a22986
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-08-09 07:45:40 +0200

    Update chargen-custom-tabs.js

--- app/components/chargen-custom-tabs.js
+++ app/components/chargen-custom-tabs.js
@@ -5,18 +5,19 @@ import { get } from '@ember/object';
 export default Component.extend({
   tagName: '',
   
-  has_connections: function() {
-      return Boolean(this.get('model.char.fs3.fs3_advantages').any(e => e == 'Connections')));
-    },
+  has_connections: computed(function() {
+      return this.get('model.char.fs3.fs3_advantages').any(e => e == 'connections');
+    }),
 
-  is_adept: computed('model', function() {
-     let myrank = this.get('model.char.ranks_rank');
-     let isAdept = false;
-     
-     if ((myrank) && (myrank == 'Adept')) {
-           isAdept = true;
-       } 
-    return isAdept;    
+  is_adept: computed(function() {
+      return (this.get('model.char.rank') == "Adept");
+//     let myrank = this.get('model.char.ranks_rank');
+//     let isAdept = false;
+//     
+//     if ((myrank) && (myrank == 'Adept')) {
+//           isAdept = true;
+//       } 
+//    return isAdept;    
   })
   
 });
