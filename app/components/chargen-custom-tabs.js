624740259770675ead34cdb64ee0e02f2be1fc2f
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-08-10 21:32:53 +0200

    Update chargen-custom-tabs.js

--- app/components/chargen-custom-tabs.js
+++ app/components/chargen-custom-tabs.js
@@ -5,16 +5,16 @@ import { get } from '@ember/object';
 export default Component.extend({
   tagName: '',
   
-  has_connections: computed(function() {
-      return this.get('model.char.fs3.fs3_advantages').any(e => e == 'connections');
+  has_connections: computed('model.char.fs3.fs3_advantages', function() {
+      return this.get('model.char.fs3.fs3_advantages').any(e => e == 'Connections');
     }),
 
   is_adept: computed('model.char.groups', function() {
-    let groups = this.get('model.char.groups');
-    if ((groups['rank'].value) && (groups['rank'].value == "Adept")) {
+//    let groups = this.get('model.char.groups');
+//    if ((groups['rank'].value) && (groups['rank'].value == "Adept")) {
        return true;
-    } else {
-      return false;
-    }
+//    } else {
+//      return false;
+//    }
   })
 });
