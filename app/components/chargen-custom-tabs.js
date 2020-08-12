9cca589594654106b6023fba744c09ab0b0913b1
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-08-12 08:17:28 +0200

    Update chargen-custom-tabs.js

--- app/components/chargen-custom-tabs.js
+++ app/components/chargen-custom-tabs.js
@@ -6,10 +6,10 @@ export default Component.extend({
   tagName: '',
   
   has_connections: computed(function() {
-     return Boolean(char.custom.cg_connections);
+     return Boolean('model.char.custom.cg_connections');
   }),
 
   is_adept: computed(function() {
-    return Boolean(char.custom.cg_adept);
+    return Boolean('model.char.custom.cg_adept');
   })
 });
