106a3921c00a53cdca403053d4f6f3ea0da4c741
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-08-12 10:39:21 +0200

    Update profile-custom-tabs.js

--- app/components/profile-custom-tabs.js
+++ app/components/profile-custom-tabs.js
@@ -1,20 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  tagName: '',
-  
-  has_marque: function() {
-      return Boolean('char.custom.marque');
-    },
-  
-  has_goals: function() {
-    let goals = this.get('model.char.custom.goals');
-    return ((goals != nil) && (goals != ""));
-  },
-  
-  no_connections: function() {
-    let con = this.get('model.char.custom.connections');
-    return ((con == nil) || (con == ""));
-  }
-  
+  tagName: ''
+
 });
